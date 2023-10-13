6f4aaddfe7fee410f17cbe2378068b8c92af33f8
zuhayr-saeed <144934369+zuhayr-saeed@users.noreply.github.com>
2023-10-12 19:10:00 -0500

    Update sketch.js

--- module-1/p5js/sketch.js
+++ module-1/p5js/sketch.js
@@ -1,3 +1,4 @@
+var f = 0;
 function setup() {
   createCanvas(windowWidth, 200);
 }
@@ -12,4 +13,16 @@ function draw() {
     stroke(255);
     point(x, y);
   }
+
+  draw=_=>{
+    f++?background(0):createCanvas(W=500,W)
+    noStroke()
+    for(x=-W;x<W;x+=5) {
+      for(n=0;n<30;n++){
+        d=(f+n+9e3)*5*noise(x)%600-50
+        100<x+d&x+d<400&100<d&d<400&&circle(x+d,d,n/6)
+    }
+    }
+  }
+ 
 }
